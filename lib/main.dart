7f1d588adc69102b9178e59041be18320ac00b10
Walid Khan <walidkhan02013@gmail.com>
2025-01-06 12:00:29 +0600

    Adding icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,45 @@
-  import 'package:flutter/material.dart';
+    import 'package:flutter/material.dart';
 
-  void main() {
-    runApp(const MyApp());
-  }
-  
-  class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-   
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor:Colors.amberAccent,
-      ),
+    void main() {
+      runApp(const MyApp());
+    }
     
-    );
-  }
+    class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+    
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          backgroundColor:Colors.amberAccent,
+          body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration( color:Colors.red,
+            borderRadius: BorderRadius.circular(20),
+            
+            
+            ),
+            padding: EdgeInsets.all(25),
+            child:Icon(
+              Icons.favorite,
+              color: Colors.white,
+            ) ,
+
+            )
+           
+
+            ),
+            
+          
+            
+            
+            ),
+            
+            
+            );
+    }
+         
 }
+    
